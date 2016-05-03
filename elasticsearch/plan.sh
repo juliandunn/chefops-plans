d017ebabc9a5d6a84d16395dfa88d61b087300b5
Ben Rockwood <benr@joyent.com>
2016-05-03 02:34:30 -0700

    ElasticSearch Plan

---
+++ elasticsearch/plan.sh
@@ -0,0 +1,26 @@
+pkg_name=elasticsearch
+pkg_origin=chefops
+pkg_version=2.3.2
+pkg_maintainer="Ben Rockwood <benr@chef.io>"
+pkg_license=('Revised BSD')
+pkg_source=https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
+pkg_filename=${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=04c4d3913d496d217e038da88df939108369ae2e78eea29cb1adf1c4ab3a000a
+pkg_deps=(core/glibc chefops/jdk8)
+pkg_bin_dirs=(bin)
+pkg_lib_dirs=(lib)
+
+pkg_service_user=elasticsearch
+pkg_service_group=elasticsearch
+
+pkg_expose=(9200 9300)
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  build_line "Copying files from $PWD"
+  cp -r * $pkg_prefix/
+}
+
