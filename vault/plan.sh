bc1b3849722c56edfad388ea2b630a1dc4935e4c
Mark Harrison <mark@mivok.net>
2016-08-12 13:55:40 -0400

    Update vault version

--- vault/plan.sh
+++ vault/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=vault
 pkg_origin=chefops
-pkg_version=0.5.3
+pkg_version=0.6.0
 pkg_maintainer="Ben Rockwood <benr@chef.io>"
 pkg_license=('MIT')
 pkg_source=https://releases.hashicorp.com/vault/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip
-pkg_shasum=fddb97507f8b539534620882f3a46984160778950e4884831c0f7c2a82b65f52
+pkg_shasum=283b4f591da8a4bf92067bf9ff5b70249f20705cc963bea96ecaf032911f27c2
 
 pkg_expose=(8200)
 
