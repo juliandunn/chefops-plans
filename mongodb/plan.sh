13c43ba73489d585e4baad7428cfb238c3a7c89e
Julian C. Dunn <jdunn@chef.io>
2016-08-18 15:49:12 -0400

    Non-working patchelf branch
    Signed-off-by: Julian C. Dunn <jdunn@chef.io>

--- mongodb/plan.sh
+++ mongodb/plan.sh
@@ -8,6 +8,7 @@ pkg_filename=${pkg_name}-linux-x86_64-${pkg_version}.tgz
 pkg_dirname=${pkg_name}-linux-x86_64-${pkg_version}
 pkg_shasum=2b977dbb222fbeddc31aecba931f726bdc488b2f8ff4314768d2fda5b27e85e3
 pkg_deps=(core/glibc core/gcc-libs)
+pkg_build_deps=(core/patchelf)
 
 pkg_expose=(27017 28017)
 # This plan uses hooks.
@@ -19,4 +20,8 @@ do_build() {
 do_install() {
   mkdir -p $pkg_prefix/bin
   cp bin/* $pkg_prefix/bin/
+
+  for i in bsondump mongodump mongoimport mongorestore mongotop mongo mongoexport mongooplog mongos mongod mongofiles mongoperf mongostat; do
+    patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" ${pkg_prefix}/bin/$i
+  done
 }
