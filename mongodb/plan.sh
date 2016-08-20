d921c8ead62056f0ae7a9e303ee520d134603240
Julian C. Dunn <jdunn@chef.io>
2016-08-19 23:54:49 -0400

    Initial packaging from source. Thanks to @miketheman for help
    Signed-off-by: Julian C. Dunn <jdunn@chef.io>

--- mongodb/plan.sh
+++ mongodb/plan.sh
@@ -4,31 +4,63 @@ pkg_version=3.2.9
 pkg_maintainer="Ben Rockwood <benr@chef.io>"
 pkg_description="High-performance, schema-free, document-oriented database"
 pkg_license=('AGPL-3.0')
+pkg_source=https://fastdl.mongodb.org/src/mongodb-src-r${pkg_version}.tar.gz
 pkg_shasum=25f8817762b784ce870edbeaef14141c7561eb6d7c14cd3197370c2f9790061b
-pkg_deps=(core/glibc core/gcc-libs)
-pkg_build_deps=(core/gcc core/scons core/python2)
-pkg_expose=(27017 28017)
+pkg_deps=(core/gcc-libs core/glibc)
+pkg_build_deps=(
+  core/coreutils
+  core/gcc
+  core/python2
+  core/scons
+)
 pkg_bin_dirs=(bin)
-# This plan uses hooks.
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+#pkg_dirname=${pkg_name}-r${pkg_version}
 
-do_build() {
-  #LD_LIBRARY_PATH=$(pkg_path_for core/gcc-libs)/lib
-  scons all
+#do_prepare() {
+#  if [[ ! -r /usr/bin/basename ]]; then
+#    ln -sv "$(pkg_path_for coreutils)/bin/basename" /usr/bin/basename
+#    _clean_basename=true
+#  fi
+#
+#  if [[ ! -r /usr/bin/tr ]]; then
+#    ln -sv "$(pkg_path_for coreutils)/bin/tr" /usr/bin/tr
+#    _clean_tr=true
+#  fi
+#
+#}
+
+do_unpack() {
+  mkdir -p $pkg_dirname
+  tar -xzf "$pkg_filename" -C $pkg_dirname --strip-components=1
+  sed -i'' -e "s#chmod 755#$(pkg_path_for coreutils)/bin/chmod 755#" $pkg_dirname/src/mongo/SConscript
 }
 
-do_strip() {
-  return 0
+do_build() {
+  CC="$(pkg_path_for gcc)/bin/gcc"
+  CXX="$(pkg_path_for gcc)/bin/g++"
+  scons --prefix="$pkg_prefix" CXX="$CXX" CC="$CC" LINKFLAGS="$LD_FLAGS" --release core
+}
+
+do_check() {
+  CC="$(pkg_path_for gcc)/bin/gcc"
+  CXX="$(pkg_path_for gcc)/bin/g++"
+  scons --prefix="$pkg_prefix" CXX="$CXX" CC="$CC" LINKFLAGS="$LD_FLAGS" --release dbtests
 }
 
 do_install() {
-  mkdir -p $pkg_prefix/bin
-  cp bin/* $pkg_prefix/bin/
-
-  for i in mongo mongos mongod ; do
-    build_line "Setting interpreter for '${pkg_prefix}/bin/${i}' '$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2'"
-    build_line "Setting rpath for '${pkg_prefix}/bin/${i}' to '$LD_RUN_PATH'"
-    patchelf --set-interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" \
-             --set-rpath ${LD_RUN_PATH} \
-             ${pkg_prefix}/bin/${i}
-  done
+  CC="$(pkg_path_for gcc)/bin/gcc"
+  CXX="$(pkg_path_for gcc)/bin/g++"
+  scons --prefix="$pkg_prefix" CXX="$CXX" CC="$CC" install
 }
+
+#do_end() {
+#  if [[ -n "$_clean_basename" ]]; then
+#    rm -fv /usr/bin/basename
+#  fi
+#
+#  if [[ -n "$_clean_tr" ]]; then
+#    rm -fv /usr/bin/tr
+#  fi
+#}
